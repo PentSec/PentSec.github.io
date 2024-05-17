e182b62bcc74754e1ca77efdd142a9ccfb13fe29
Jeff. <pentsec.2@protonmail.com>
2024-05-17 16:55:22 -0400

    edit

---
+++ scripts/allthings.js
@@ -0,0 +1,483 @@
+let homeContent = `<div class="content-wrapper">
+<section class="home ">
+    <div class="greetings content-card">
+        <div class="greetings-title">
+            <h1 class="t1"> HI </h1>
+            <h1 class="t2"> My Name is <span>Jeffreys</span> aka Jeff. </h1>
+            <h1 class="t3"> I am a amateur programmer </h1>
+        </div>
+
+    </div>
+
+</section>`;
+
+let aboutContent = `<section class="about-wrapper">
+<div class="about-section about-content-card">
+  <div class="about-content">
+    <div class="about-title">
+      <h1>About</h1>
+    </div>
+    <div class="about-text">
+      <p>
+        My name is Jeffreys. Hacktivist and Old Whitehack<br />
+        <br />
+        In 2011 I decided to change my career.
+        I wanted to learn about what I was really interested in and what I enjoyed doing in my free time<br />
+        so I began putting effort into programming and coding.<br />
+        That same year, 2011, I quit my job and dedicated myself completely to study for hardware and software bugs fixes.<br />
+        <br />
+       
+        I began with Lua and
+        <i class="fab fa-python"></i> I created some scripts in LUA for an emulator called ArcEmu one of the first 
+        emulators to create private world of warcraft servers. <br />
+        Later I was introduced to C/C++ when I discovered the TrinityCore code, I was 
+        experimenting for months, modified and played with the code as I pleased, then I left it all behind.<br />
+        Few months later I came back this time to IRC ,there were many helpful channels and also many bots and I got interested in them.
+        and I met <i class="fab fa-python"></i>, <br />
+        <br />
+        I created some bots for IRC and XMPP I also created a
+        tool for pentesting without doubt became my favorite programming language.
+        <br />
+      </p>
+    </div>
+  </div>
+</div>
+</section>`;
+
+let workContent = `
+
+        <div class="work-content work-content-card">
+            <div class="work-gallery">
+
+                <div class="work-project-card card-6">
+                    <h1 class="work-project-card-title tooltip ">
+                    TITULO AQUI  <span class="tooltiptext">
+                    Project idea: 
+                    <br>-IDEA1 
+                    <br>-IDEA2
+                    
+                    </span>
+                    </h1>
+                    <a href="repositorieshere" target="_blank">
+                    <img class="workPic tooltip" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere">  
+                    <p class="button-project btn-github ">Go to GITHUB </p></a>
+                </div>
+
+                <div class="work-project-card card-5">
+                    <h1 class="work-project-card-title tooltip">
+                    TITULO AQUI  <span class="tooltiptext">
+                    Project idea: 
+                    <br>-IDEA1 
+                    <br>-IDEA2
+                        
+                        </span>
+                    </h1>
+                    <a href="repositorieshere" target="_blank">
+                    <img class="workPic" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-4">
+                        <h1 class="work-project-card-title tooltip">
+                        TITULO AQUI  <span class="tooltiptext">
+                        Project idea: 
+                        <br>-IDEA1 
+                        <br>-IDEA2
+                        
+                        </span>
+                    </h1>
+                        <a href="repositorieshere/" target="_blank">
+                    <img class="workPic" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-3">
+                    <h1 class="work-project-card-title tooltip">
+                        TITULO AQUI  <span class="tooltiptext">
+                        Project idea: 
+                        <br>-IDEA1 
+                        <br>-IDEA2 
+                        
+                        </span>
+                    </h1>
+                    <a href="repositorieshere" target="_blank">
+                    <img class="workPic" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                    <h1 class="work-project-card-title tooltip">
+                        TITULO AQUI  <span class="tooltiptext">
+                        Project idea: 
+                        <br>-IDEA1 
+                        <br>-IDEA2
+                        </span>
+                    </h1>
+                    <a href="repositorieshere" target="_blank">
+                    <img class="workPic" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                <div class="work-project-card card-2">
+                    <h1 class="work-project-card-title tooltip">
+                        TITULO AQUI  <span class="tooltiptext">
+                        Project idea: 
+                        <br>-IDEA1 
+                        <br>-IDEA2                       
+                        
+                        </span>
+                    </h1>
+                    <a href="repositorieshere" target="_blank">
+                    <img class="workPic" src="https://cdn.discordapp.com/attachments/1241027109271371776/1241061573100048414/41c66ce3-4e8f-4dff-8cd6-85cc5e520198.jpeg?ex=6648d428&is=664782a8&hm=26e89b0999c97ba9917b7ea3451ab39f91038d264227505b8e8cb76c594cae6e&"> </a>
+                    <a href="repositorieshere" target="_blank">  
+                    <p class="button-project btn-github ">Go to GITHUB</p>
+                    </a>
+                </div>
+
+                
+            </div>
+            <p class="mobile">GO MOBILE</p>
+        </div>
+      
+  
+`;
+
+let skillsContent = `<div class="skills-section">
+<div class="skills-content skills-content-card">
+    <div class="skills-title">
+        <h1>Skills</h1>
+    </div>
+    <div class="skills-text">
+        <p> My main expertise is the programming and development of AddOns and Bots, I also specialize in hardware and software bug fixes.</p>
+        <p> using<br/>- LUA, XML, PYTHON, PERL, JS, c#, C++</p>
+        <p>I also use:</p>
+        <p>- MySQL , HTML ,<br/>
+         - CSS</p>
+
+        <p>- GSAP, Anime.js, ScrollMagic, SASS, <br/>
+        - Bootstrap , GIT,<br/></p>
+
+    </div>
+</div>
+</div>`;
+
+let mobileContent = `<section class="work-wrapper">
+<div class="work-content work-content-card">
+    <div class="work-gallery">
+
+        <div class="work-project-card card-6">
+            <h1 class="work-project-card-title ">
+            TITULO AQUI
+            </h1>
+            <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/realestateQR.jpg"> </a>
+            <a href="repositorieshere">  
+            <p class="button-project btn-github ">Go to GITHUB</p></a>
+        </div>
+
+        <div class="work-project-card card-5">
+            <h1 class="work-project-card-title ">
+            TITULO AQUI
+            </h1>
+            <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/digitalagensyQR.jpg"> </a>
+            <a href="repositorieshere" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-4">
+                <h1 class="work-project-card-title ">
+                TITULO AQUI
+            </h1>
+                <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/paralaxMountinQR.jpg"> </a>
+            <a href="repositorieshere" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-3">
+            <h1 class="work-project-card-title ">
+                TITULO AQUI
+            </h1>
+            <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/tezgaShopQR.jpg"> </a>
+            <a href="repositorieshere" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-2">
+            <h1 class="work-project-card-title ">
+                TITULO AQUI
+            </h1>
+            <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/burgelicious.jpg"> </a>
+            <a href="repositorieshere" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        <div class="work-project-card card-1">
+            <h1 class="work-project-card-title ">
+                TITULO AQUI
+            </h1>
+            <a href="repositorieshere" target="_blank">
+            <img class="qr-card" src="./QRprojects/movieSearchQR.jpg"> </a>
+            <a href="repositorieshere" target="_blank">  
+            <p class="button-project btn-github ">Go to GITHUB</p>
+            </a>
+        </div>
+
+        
+    </div>
+    <p class="Works">GO WORKS</p>
+</div>
+
+</section>`;
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const work = document.querySelector("#work");
+const skills = document.querySelector("#skills");
+let contentBox = document.querySelector(".content-wrapper");
+let phoneContact = document.querySelector(".letsTalk");
+let enter = document.querySelector(".preloader-wrapper");
+
+document.addEventListener("DOMContentLoaded", () => {
+  let enterSequence = new TimelineMax({});
+
+  contentBox.innerHTML = "";
+  contentBox.innerHTML = homeContent;
+  enterSequence
+    .from(".letsTalk", 1, {
+      x: -300
+    })
+    .from("#home", 0.1, {
+      opacity: 0
+    })
+    .from("#home", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#about", 0.1, {
+      opacity: 0
+    })
+    .from("#about", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#work", 0.1, {
+      opacity: 0
+    })
+    .from("#work", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from("#skills", 0.1, {
+      opacity: 0
+    })
+    .from("#skills", 0.3, {
+      y: 400,
+      fontSize: 250
+    })
+    .from(".home", 1, {
+      opacity: 0,
+      x: -300
+    })
+    .from(".t1", 0.1, {
+      opacity: 0
+    })
+    .from(".t2", 0.1, {
+      opacity: 0
+    })
+    .from(".t3", 0.1, {
+      opacity: 0
+    })
+    .from(".interface-settings li", 0.9, {
+      x: 550
+    });
+
+  home.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = homeContent;
+    let openHome = new TimelineMax({});
+    openHome
+      .from(".home", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(".t1", 0.1, {
+        opacity: 0
+      })
+      .from(".t2", 0.1, {
+        opacity: 0
+      })
+      .from(".t3", 0.1, {
+        opacity: 0
+      });
+  });
+  about.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = aboutContent;
+    let openAbout = new TimelineMax({});
+    openAbout
+      .from(".about-section", 1, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".about-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".about-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".about-text p", 0.2, {
+        opacity: 0
+      });
+  });
+  work.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = workContent;
+    let openWork = new TimelineMax({});
+
+    openWork
+      .from(".work-content-card", 1, {
+        opacity: 0,
+        x: -300
+      })
+      .from(
+        ".card-5",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-2",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-3",
+        0.1,
+        {
+          opacity: 0
+        },
+        "op"
+      )
+      .from(
+        ".card-6",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      )
+      .from(
+        ".card-4",
+        0.1,
+        {
+          opacity: 0
+        },
+        "oz"
+      );
+
+      let goMobile = document.querySelector(".mobile");
+    goMobile.addEventListener("click", () => {
+      contentBox.innerHTML = "";
+      contentBox.innerHTML = mobileContent;
+    });
+  }); 
+
+      let goWorks = document.querySelector(".Works");
+    goWorks.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = workContent;
+  });
+}); 
+
+  skills.addEventListener("click", () => {
+    contentBox.innerHTML = "";
+    contentBox.innerHTML = skillsContent;
+    let openSkills = new TimelineMax({});
+    openSkills
+      .from(".skills-content", 0.6, {
+        opacity: 0,
+        x: 300
+      })
+      .from(
+        ".skills-title h1",
+        0.1,
+        {
+          opacity: 0
+        },
+        "ww"
+      )
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 1
+      })
+      .from(".skills-title h1", 0.1, {
+        opacity: 0
+      })
+      .from(
+        ".skills-text",
+        0.2,
+        {
+          opacity: 0
+        },
+        "ww",
+        1
+      );
+  });
+  let openContact = new TimelineMax({ paused: true, reversed: true });
+  phoneContact.addEventListener("click", () => {
+    phoneContact.classList.toggle("opened");
+
+    openContact.reversed() ? openContact.play() : openContact.reverse();
+
+    openContact.to(".interface-settings li", 0.3, {
+      x: 550
+    });
+  });
